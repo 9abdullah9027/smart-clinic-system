fd15660941482d807ff8ebe4ed56d70648c93a50
9abdullah9027 <9abdullah9027@gmail.com>
2025-11-18 16:30:31 +0500

    Changes in frontend

---
+++ frontend/src/routes/AppRoutes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import Appointments from "../pages/Appointments";
+
+const AppRoutes = () => (
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/appointments" element={<Appointments />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRoutes;
